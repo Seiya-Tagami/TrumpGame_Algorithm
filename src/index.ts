e04341fe24e6ce6f61bf320a558b2ac876449120
Seiya <107479598+Seiya-Tagami@users.noreply.github.com>
2023-07-01 14:34:49 +0000

    feat: startGame

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,83 @@
-const Hello = () => {
-    console.log("Hello2")
+class Card {
+    suit: string
+    value: string
+    intValue: number
+    
+    constructor(suit: string, value: string, intValue: number) {
+        this.suit = suit;
+        this.value = value;
+        this.intValue = intValue;
+    }
+
+    getCardString() {
+        return this.suit + this.value + "(" + this.intValue + ")";
+    }
 }
 
-Hello()
+class Deck {
+    deck: Card[]
+
+    constructor() {
+        this.deck = Deck.generateDeck();
+    }
+
+    static generateDeck() {
+        let newDeck = [];
+        const suits = ["♣", "♦", "♥", "♠"];
+        const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+        for (let i = 0; i < suits.length; i++) {
+            for (let j = 0; j < values.length; j++) {
+                newDeck.push(new Card(suits[i], values[j], j + 1));
+            }
+        }
+        return newDeck;
+    }
+
+    draw() {
+        return this.deck.pop() as unknown as Card
+    }
+
+    printDeck() {
+        console.log("Displaying cards...")
+        for(let i = 0; i < this.deck.length; i++) {
+            console.log(this.deck[i].getCardString())
+        }
+    }
+
+    shuffleDeck() {
+        let deckSize = this.deck.length;
+        for(let i = deckSize - 1; 0 <= i; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = this.deck[i];
+            this.deck[i] = this.deck[j];
+            this.deck[j] = temp;
+        }
+    }
+}
+
+class Dealer {
+    static startGame(amountOfPlayers: number) {
+        let table = {
+            "players": [] as Card[][],
+            "deck": new Deck()
+        }
+
+        table["deck"].shuffleDeck()
+
+        for(let i = 0; i < amountOfPlayers; i++) {
+            let playerCard = []
+            // ブラックジャックの手札は2枚
+            for (let j = 0; j < 2; j++) {
+                playerCard.push(table["deck"].draw());
+            }
+
+            table["players"].push(playerCard);
+        }
+
+        return table["players"]
+    }
+}
+
+let table1 = Dealer.startGame(4);
+console.log(table1);
